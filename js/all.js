a49f61865f704b990a40932f88a7bd4763d60721
Ali-Owieda92 <owiedaali92@gmail.com>
2024-06-28 18:53:28 +0300

    Add Extra Projects

---
+++ js/all.js
@@ -0,0 +1,17 @@
+let links = document.querySelectorAll(".projects .card");
+
+let mainLink = document.querySelector(".projects .show a");
+let mainImg = document.querySelector(".projects .show a img");
+let mainHeading = document.querySelector(".projects .show a .text h2");
+let paragraph = document.querySelector(".projects .show a .text p");
+
+
+links.forEach((link)=> {
+    link.onclick = (e) => {
+        e.preventDefault();
+        mainLink.href = link.href;
+        mainImg.src = link.firstElementChild.src;
+        mainHeading.innerHTML = link.lastElementChild.firstElementChild.innerHTML;
+        paragraph.innerHTML = link.lastElementChild.lastElementChild.innerHTML;
+    }
+})
--- js/portfolio.js
+++ js/portfolio.js
@@ -7,3 +7,29 @@ links.forEach((link) => {
         e.currentTarget.classList.add("active");
     })
 });
+
+let buttons = document.querySelectorAll(".button button"); // All Buttons
+
+let js = document.querySelector(".button .jbtn"); // JavaScirpt Button
+let re = document.querySelector(".button .rbtn"); // React Button
+
+let javascript = document.querySelector(".cards.javascript"); // JavaScript Card
+let react = document.querySelector(".cards.react"); // React Card
+
+buttons.forEach((b) => {
+    b.addEventListener('click',() => {
+        buttons.forEach((b) => {
+            b.classList.remove("active");
+        })
+        b.classList.add("active");
+    })
+})
+
+re.onclick = function() {
+    javascript.style.display = 'none';
+    react.style.display = 'grid';
+}
+js.onclick = function() {
+    react.style.display = 'none';
+    javascript.style.display = 'grid';
+}
